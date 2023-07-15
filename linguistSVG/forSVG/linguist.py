cc896e9c06271f65eb242e97e00106bc4197a7c6
tetraloba <four.leaves.siva7342@gmail.com>
2023-07-15 16:36:52 +0900

    delete_debug_output

--- linguistSVG/forSVG/linguist.py
+++ linguistSVG/forSVG/linguist.py
@@ -31,16 +31,12 @@ svg.rect(0, 0, '100%', '100%', background_color, stroke_width=0)
 pen_x = 0
 for lang, size in sizes_list:
     r, g, b = settings['langs'][lang][0] # todo 例外処理すべき
-    # print(f'\033[48;2;{r};{g};{b}m' + ' ' * int(round(size, '0')) + '\033[0m', end='')
     svg.line(pen_x, char_size / 2, pen_x + size * width, char_size / 2, RGB(r, g, b), width=char_size)
     pen_x += size * width
-print()
 # 言語とその割合を表示
 for i, (lang, size) in enumerate(sizes_list):
     if size != 0:
         r, g, b = settings['langs'][lang][0] # todo 例外処理すべき
-        print(f'\033[38;2;{r};{g};{b}m●\033[0m {lang} {size}%')
         svg.circle(char_size / 2, bar_height + char_size * i + char_size / 2, char_size / 2, RGB(r, g, b), stroke_width=0)
         svg.text(char_size, bar_height + char_size * i + char_size, f"{lang} {round(size * 100, '0.1')}%", font_size=char_size, fill_color=RGB(255, 255, 255), stroke_width=0)
-print()
 
